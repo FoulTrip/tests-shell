9cab4ccc7dcb8033ef1803d25e23b0809f56055c
David Vasquez <83887793+FoulTrip@users.noreply.github.com>
2023-08-18 10:59:54 -0500

    act003.3

--- tests/environment/delete-env.c
+++ tests/environment/delete-env.c
@@ -29,37 +29,6 @@ int main()
 {
 	setenv("TEST_VARIABLE", "hello, World", 1);
 
-	printf("Before removal: TEST_VAR=%s\n", getenv("#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-
-extern char **environ;
-
-int _unsetenv(const char *name)
-{
-	int i, j;
-
-	if (name == NULL || name[0] == '\0' || strchr(name, '=') != NULL)
-
-	for (i = 0; environ[i] != NULL; ++i)
-	{
-		if (strncmp(name, environ[i], strlen(name)) == 0 && environ[i][strlen(name)] == '=')
-		{
-			for (j = i; environ[j] != NULL; ++j)
-			{
-				environ[j] = environ[j + i];
-			}
-			return (0);
-		}
-	}
-
-	return (-1);
-}
-
-int main()
-{
-	setenv("TEST_VARIABLE", "hello, World", 1);
-
 	printf("Before removal: TEST_VARIABLER=%s\n", getenv("TEST_VARIABLE"));
 
 	_unsetenv("TEST_VARIABLE_CPY");
